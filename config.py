cab6aa3c97cf383805bd737477d4a4dcaa323ee5
TonaAkerele <tona.information@gmail.com>
2025-02-10 20:19:32 -0600

    Update config.py
    updating config.py

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 import os
-
 from urllib.parse import quote_plus
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'm768Q~SV~bq_AEpWSA1MEyBuzyB5aZbk22gtSaxt'
@@ -14,15 +12,25 @@ class Config(object):
     SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'cms'
     SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'servercms'
     SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'Udacitystudent@'
-# SQL Connection String
+# Connection string with timeout and connection pooling
     params = quote_plus(
         'DRIVER={ODBC Driver 17 for SQL Server};' +
         f'SERVER={SQL_SERVER};' +
         f'DATABASE={SQL_DATABASE};' +
         f'UID={SQL_USER_NAME};' +
-        f'PWD={SQL_PASSWORD}'
+        f'PWD={SQL_PASSWORD};' +
+        'Connection Timeout=30;' +
+        'Command Timeout=30;' +
+        'Pooling=true;' +
+        'Min Pool Size=5;' +
+        'Max Pool Size=30'
     )
     SQLALCHEMY_DATABASE_URI = "mssql+pyodbc:///?odbc_connect=%s" % params
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        'pool_size': 10,
+        'pool_timeout': 30,
+        'pool_recycle': 1800
+    }
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 # Microsoft Authentication
     CLIENT_SECRET = os.environ.get('CLIENT_SECRET') or "goe8Q~ynpD_zoAEXA3feMphz4rNOo1H0zYqJdaif"
