61409a190e01a140ada2699b697be2256966d061
TonaAkerele <tona.information@gmail.com>
2025-02-13 16:48:11 -0600

    Update views.py
    update logger success and fail to be the same

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -71,7 +71,7 @@ def login():
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
-        app.logger.info('Successful login for user: %s', user.username)
+        app.logger.info('Successful login for user: %s', form.username.data)
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
         return redirect(next_page)
