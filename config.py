1a0b61962962980a6324e20bf3cb37990f8305b2
TonaAkerele <tona.information@gmail.com>
2025-02-13 13:10:37 -0600

    Update config.py
    moving select account portion location

--- config.py
+++ config.py
@@ -37,6 +37,7 @@ class Config(object):
     # You can find the proper permission names from this document
     # https://docs.microsoft.com/en-us/graph/permissions-reference
     SCOPE = ["User.Read"] # Only need to read user profile for this app
-
-    SESSION_TYPE = "filesystem"  # Token cache will be stored in server-side session
     PROMPT = "select_account"
+    
+    SESSION_TYPE = "filesystem"  # Token cache will be stored in server-side session
+    
