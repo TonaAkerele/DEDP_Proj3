d0c1304e8e72fb11433eab6e4d4336a3c06e1a0a
TonaAkerele <tona.information@gmail.com>
2025-02-13 11:36:33 -0600

    Update views.py
    notes about logging

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -66,10 +66,11 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
+            flash('Invalid username or password') #add a loggin/logger here to say/show it failed
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
+        #add login successful here where it's successful
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
         return redirect(next_page)
